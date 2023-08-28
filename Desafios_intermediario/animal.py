59c44b95549b6c9426944395bea399d67dd4b5ba
Raphaellaurentino <117394856+Raphaellaurentino@users.noreply.github.com>
2023-08-28 14:26:06 -0300

    Create animal.py

---
+++ Desafios_intermediario/animal.py
@@ -0,0 +1,23 @@
+
+a = input() 
+b = input() 
+c = input() 
+
+if a == 'vertebrado'and b == 'ave'and c == 'carnivoro':
+    print('Aguia')    
+elif a == 'vertebrado'and b == 'ave'and c == 'onivoro':
+    print('Pomba')
+elif a == 'vertebrado'and b == 'mamifero'and c == 'onivoro':
+    print('Homen')
+elif a == 'vertebrado'and b == 'mamifero'and c == 'herbivoro':
+    print('Vaca')
+elif a == 'invertebrado'and b == 'inseto'and c == 'hematofago':
+    print('Pulga')
+elif a == 'invertebrado'and b == 'inseto'and c == 'herbivoro':
+    print('Lagarta')
+elif a == 'invertebrado'and b == 'anelideo'and c == 'hematofago':
+    print('Sanguessuga')
+elif a == 'invertebrado'and b == 'anelideo'and c == 'onivoro':
+    print('Minhoca')
+
+
